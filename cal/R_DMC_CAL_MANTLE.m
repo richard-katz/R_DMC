41c2a15dca5693b72d8907a452950357bf426002
Tobias Keller <2519795+tokeller@users.noreply.github.com>
2019-05-14 15:59:30 -0700

    update example calibration and user files for 5-component mantle melting

--- cal/R_DMC_CAL_MANTLE.m
+++ cal/R_DMC_CAL_MANTLE.m
@@ -2,7 +2,7 @@
 %*****  R_DMC CALIBRATION EXAMPLE FILE  ***********************************
 %**************************************************************************
 %
-% COPIRIGHT  (C) 2015
+% COPIRIGHT (C) 2015
 % 	  Tobias Keller [tobias.keller@earth.ox.ac.uk] 
 % 	  University of Oxford
 % 	  FOALAB
@@ -11,13 +11,10 @@
 % 	  Oxford, OX1 3AN
 % 	  United Kingdom
 %
-% PROJECT    R_DMC
-% FILENAME   R_DMC_Cal_HydrCarbMantle.m
+% PROJECT   R_DMC
+% FILENAME  R_DMC_Cal_HydrCarbMantle.m
 % 
-% REFERENCE  Keller, T & Katz, R F (2015) Effects of volatiles on melt 
-% production and reactive flow in the mantle. arXiv:1510.01334v1.
-
-% LICENSE    R_DMC is free software: you can redistribute it and/or modify 
+% LICENSE:  R_DMC is free software: you can redistribute it and/or modify 
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, version 3.
 % 
@@ -30,63 +27,61 @@
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
 % 
 %
-% HELP:  This file contains an example calibration for hydrated and carbonated
-% mantel melting in a four-component compositional space of dunite, MORB,
-% hydrated MORB-type silicate with 5 wt % H2O, and carbonated MORB-type
-% silicate with 20 wt % CO2.
+% HELP:  This file contains a test calibration for idealised, linearised 
+% mantle melting in a five-component compositional space, with components 
+% c1,...,c5 sorter in order of increasing incompatibility and decreasing 
+% concentration.
 
 % set number of components for R_DMC method  [USER SPEC]
-PAR.nc  =  6;
+PAR.nc  =  5;
 
 % set string with component name to appear in figure legends
-PAR.CompStr  =  {'Fo90~','Opx~','Cpx~','CaAlSi~','HSi~','CSi~'};
+PAR.CompStr  =  {'for','fay','opx','cpx','vol'};
 
 % specify calibration parameters for all components below [USER SPEC]
 
 % set pure component melting points T_m^i at P=0
-PAR.T0(1)  =  1780;                  
-PAR.T0(2)  =  1200;
-PAR.T0(3)  =  1050;
-PAR.T0(4)  =  850;
-PAR.T0(5)  =  710;
-PAR.T0(6)  =  640;
+PAR.T0(1)  =  1890;
+PAR.T0(2)  =  1205;
+PAR.T0(3)  =  1175;
+PAR.T0(4)  =  1000;
+PAR.T0(5)  =   800;
 
 % choose type of parameterisation for T_m^i(P)
-PAR.Tm_P_mode  = 'quadratic';
+PAR.Tm_P_mode  = 'simonslaw';
 
-% set coeff. for linear P-dependence of T_m^i [K/GPa]
-PAR.A(1)   =  45;
-PAR.A(2)   =  90;
-PAR.A(3)   =  100;
-PAR.A(4)   =  90;
-PAR.A(5)   =  40.8;
-PAR.A(6)   =  30.1;
+% set first coeff. for P-dependence of T_m^i [GPa]
+PAR.A(1)   =   10.83;
+PAR.A(2)   =   15.78;
+PAR.A(3)   =   4.6;
+PAR.A(4)   =   1.8;
+PAR.A(5)   =   5.6;
 
-% set coeff. for quadratic P-dependence of T_m^i [K/GPa^2]
-PAR.B(1)   =  -2.0;
-PAR.B(2)   =  -2.5;
-PAR.B(3)   =  -3.0;
-PAR.B(4)   =  -2.5;
-PAR.B(5)   =  -1.54;
-PAR.B(6)   =  -1.88;
+% set second coeff. for P-dependence of T_m^i [1]
+PAR.B(1)   =  3.7;
+PAR.B(2)   =  1.6;
+PAR.B(3)   =  2.6;
+PAR.B(4)   =  3;
+PAR.B(5)   =  2;
 
-% set latent heat of pure components L^i [J/kg]
-PAR.L(1)   =  600e3;
-PAR.L(2)   =  450e3;
-PAR.L(3)   =  400e3;
-PAR.L(4)   =  350e3;
-PAR.L(5)   =  350e3;
-PAR.L(6)   =  350e3;
+% set entropy gain of fusion DeltaS [J/K]
+PAR.dS     =  330;
+
+% get component latent heat of fusion L^i = dS*T_0^i
+PAR.L(1)   =  870e3;%(PAR.T0(1)+273.15)*PAR.dS;
+PAR.L(2)   =  520e3;%(PAR.T0(2)+273.15)*PAR.dS;
+PAR.L(3)   =  470e3;%(PAR.T0(3)+273.15)*PAR.dS;
+PAR.L(4)   =  420e3;%(PAR.T0(4)+273.15)*PAR.dS;
+PAR.L(5)   =  350e3;%(PAR.T0(5)+273.15)*PAR.dS;
 
 % choose type of parameterisation for K^i(T)
 PAR.K_T_mode  = 'inverse_exp';
 
-% set coeff. for T-dependence of distribution coefficients K^i
-PAR.r(1)   =  70;
-PAR.r(2)   =  28;
-PAR.r(3)   =  26;
-PAR.r(4)   =  24;
-PAR.r(5)   =  30;
-PAR.r(6)   =  30;
+% set coeff. for T-dependence of partition coefficients K^i [1/K]
+PAR.r(1)   =  80;
+PAR.r(2)   =  60;
+PAR.r(3)   =  8;
+PAR.r(4)   =  10;
+PAR.r(5)   =  12;
 
 
